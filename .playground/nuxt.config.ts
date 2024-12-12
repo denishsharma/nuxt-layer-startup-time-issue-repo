4d4ef1d05a741fd61b63495f7b075c08744db37e
Denish Sharma <denishcommon@gmail.com>
2024-12-12 13:54:27 +0530

    chore: base layer project

---
+++ .playground/nuxt.config.ts
@@ -0,0 +1,16 @@
+import { fileURLToPath } from 'node:url'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-12-12',
+  future: {
+    compatibilityVersion: 4
+  },
+  extends: ['..'],
+  modules: ['@nuxt/eslint'],
+  eslint: {
+    config: {
+      // Use the generated ESLint config for lint root project as well
+      rootDir: fileURLToPath(new URL('..', import.meta.url))
+    }
+  },
+})
